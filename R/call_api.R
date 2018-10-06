31edf9b3a6f49816b548af91ee08dcb83927c052
davesteps <davesteps@gmail.com>
2018-10-06 23:28:33 +0100

    dummy call

---
+++ R/call_api.R
@@ -0,0 +1,53 @@
+
+#' api_call
+#'
+#' @param api_info
+#' @param out_dir
+#'
+#' @return
+#' @export
+#'
+#' @examples
+api_call <- function(out_dir,api_info=''){
+  df <- fetchData(api_info)
+  df <- cleanData(df)
+  writeData(df,out_dir)
+}
+
+#' fetchData
+#'
+#' @param api_info
+#'
+#' @return
+#' @export
+#'
+#' @examples
+fetchData <- function(api_info=''){
+  iris
+}
+
+#' cleanData
+#'
+#' @return
+#' @export
+#'
+#' @examples
+cleanData <- function(df){
+  df
+}
+
+#' writeData
+#'
+#' @param dir
+#'
+#' @return
+#' @export
+#'
+#' @examples
+writeData <- function(df,out_dir='.'){
+  # df <- iris
+  fn <- file.path(out_dir,paste0(Sys.Date(),'.csv'))
+  write.table(df,fn,append = new_file,col.names = F,row.names = F)
+}
+
+
