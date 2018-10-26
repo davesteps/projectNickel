6aca989837a0a4d4136aa46b2ac2373ec695a5f4
davesteps <davesteps@gmail.com>
2018-10-26 20:03:32 +0100

    basic

--- R/call_api.R
+++ R/call_api.R
@@ -1,54 +1,126 @@
 
-#' api_call
+
+filename <- function(ext,level){
+  now <- lubridate::now(tzone = "UTC")
+  paste0(as.Date(now),'-',lubridate::hour(now),'-',lubridate::minute(now),ext)
+}
+
+#' bucketKeys
 #'
-#' @param api_info
-#' @param out_dir
+#' @param bucket
+#' @param ext
 #'
 #' @return
 #' @export
 #'
 #' @examples
-api_call <- function(out_dir,api_info=''){
-  df <- fetchData(api_info)
-  df <- cleanData(df)
-  writeData(df,out_dir)
+bucketKeys <- function(bucket,ext='bz2'){
+  # given a bucket and extension
+  # returns data frame with object keys and datetimes
+  try({
+    bl <- aws.s3::get_bucket(bucket)
+    df <- plyr::ldply(bl,function(b){
+      data.frame(name=b$Key,
+                 time=(b$LastModified),
+                 stringsAsFactors = F)})
+    df$time <- as.POSIXct(df$time,format = '%Y-%m-%dT%H:%M:%S',tz = 'UTC')
+    df
+
+  },silent = T)
 }
 
-#' fetchData
+is.err <- function(x) inherits(x,"try-error")
+
+#' sendToS3
 #'
-#' @param api_info
+#' @param obj
+#' @param bucket
+#' @param tryN
 #'
 #' @return
 #' @export
 #'
 #' @examples
-fetchData <- function(api_info=''){
-  iris
+sendToS3 <- function(obj,bucket,tryN = 5){
+
+  put <- try(aws.s3::put_object(obj,bucket = bucket),silent = T)
+
+  i <- 0
+  while(is.err(put) & i<tryN){
+    # flog.warn('No rows in data.frame')
+    Sys.sleep(i)
+    put <- try(aws.s3::put_object(obj,bucket = bucket),silent = T)
+    i <- i+1
+  }
+  put
 }
 
-#' cleanData
+
+#' aisToS3
+#'
+#' @param keys
 #'
 #' @return
 #' @export
 #'
 #' @examples
-cleanData <- function(df){
-  df
+aisToS3 <- function(keys){
+
+  Sys.setenv("AWS_ACCESS_KEY_ID" = keys$AWS_ACCESS_KEY_ID,
+             "AWS_SECRET_ACCESS_KEY" = keys$AWS_SECRET_ACCESS_KEY,
+             "AWS_DEFAULT_REGION" = keys$AWS_DEFAULT_REGION)
+
+
+  fn <- filename('.bz2')
+  download.file(keys$AISHUB_URL,destfile = fn)
+  on.exit(file.remove(fn))
+
+  sendToS3(fn,'ais-current',5)
+
 }
 
-#' writeData
+#' aggregateAIS
 #'
-#' @param dir
+#' @param keys
 #'
 #' @return
 #' @export
 #'
 #' @examples
-writeData <- function(df,out_dir='.'){
-  # df <- iris
-  fn <- file.path(out_dir,paste0(Sys.Date(),'.csv'))
-  new_file <- ifelse(file.exists(fn),T,F)
-  write.table(df,fn,append = new_file,col.names = F,row.names = F)
+aggregateAIS <- function(keys){
+
+  Sys.setenv("AWS_ACCESS_KEY_ID" = keys$AWS_ACCESS_KEY_ID,
+             "AWS_SECRET_ACCESS_KEY" = keys$AWS_SECRET_ACCESS_KEY,
+             "AWS_DEFAULT_REGION" = keys$AWS_DEFAULT_REGION)
+
+  kl <- bucketKeys('ais-current','.bz2')
+
+  if(is.err(kl) || !nrow(kl)) return()
+
+  # only get keys from last 61mins
+  cutoff <- lubridate::now(tzone = "UTC") - (61*60)
+  kl <- kl[kl$time > cutoff,]
+
+  if(!nrow(kl)) return()
+
+  kl <- kl$name
+
+  tl <- lapply(kl,function(k) try(s3read_using(readr::read_csv,object=k,bucket='ais-current'),silent = T))
+
+  # filter out errors and (remove from kl)
+  errors <- unlist(lapply(tl, is.err))
+  kl <- kl[!errors]
+  tl <- tl[!errors]
+
+  br <- try(bind_rows(tl) %>% unique(),silent = T)
+
+  # if br is not err carry on
+  if(is.err(br)) return()
+
+  fn <- filename('.bz2')
+  readr::write_csv(br,fn)
+  on.exit(file.remove(fn))
+  put <- sendToS3(fn,'ais-archive',10)
+  if(!is.err(put)) for(k in kl) aws.s3::delete_object(k,bucket = 'ais-current')
+
 }
-
-
